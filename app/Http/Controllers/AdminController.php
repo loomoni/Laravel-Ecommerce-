39e07b9329164bb5d14b25b17e742e141e18d590
loomoni <loomonimorwo1@gmail.com>
2025-01-30 12:49:34 +0300

    added admin category page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Carbon\Carbon;
 use App\Models\Brand;
+use App\Models\Category;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
@@ -104,5 +105,12 @@ class AdminController extends Controller
         $brand->delete();
 
         return redirect()->route('admin.brands')->with('status', 'brand has been deleted successfully');
+    }
+
+    public function categories()
+    {
+        $categories = Category::orderBy('id', 'DESC')->paginate(10);
+
+        return view('admin.categories', compact('categories'));
     }
 }
